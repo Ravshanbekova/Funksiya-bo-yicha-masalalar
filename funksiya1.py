77c5fcf99b23837f0ec98c413dfa2cdb07437f65
Zuhrajon <zuhrajon.axv@gmail.com>
2025-12-09 16:48:08 +0500

    Add files via upload

---
+++ funksiya1.py
@@ -0,0 +1,36 @@
+# # 1 - masala
+# def kvadrat(son):
+#         return son * son
+# print(kvadrat(5))
+
+
+# 2 - masala
+def takror_matn(matn):
+    natija = matn + matn + matn
+    return f"Takror matn: {natija}"
+matn = str(input("Matinni kiriting: "))
+print(takror_matn(matn))
+
+
+#
+# # 4 - masala
+# def yigindi(son1, son2):
+#     natija = son1 + son2
+#     return f"Yig'indi: {natija}"
+#
+# son1 = int(input("Son 1 ni kiriting: "))
+# son2 = int(input("Son 2 ni kiriting: "))
+#
+# print(yigindi(son1, son2))
+
+
+
+
+
+
+
+
+
+
+
+
